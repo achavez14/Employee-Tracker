0a605b92da25d5604383b52cfee3563916abfc19
Alexa Chavez <aguinada14@gmail.com>
2024-05-05 18:26:55 -0700

    Implemented employee and role management functionality

--- db/queries.sql
+++ db/queries.sql
@@ -8,7 +8,7 @@ INNER JOIN department d ON r.department_id = d.id;
 
 -- Query to view all employees
 SELECT e.id, e.first_name. e.last_name, r.title AS job_title, d.name AS department, r.salary, CONCAT(m.first_name, '', m.last_name) AS manager_id
-FROM employee employee e
+FROM employee e
 INNER JOIN role r ON e.role_id = r.id
 INNER JOIN department d ON r.department_id = d.id
 LEFT JOIN employee m ON e.manager_id = m.id;
--- index.js
+++ index.js
@@ -21,22 +21,96 @@ function mainMenu() {
             mainMenu(); // Return to main menu
           });
           break;
-        case 'Add employee':
-          // Implement logic to add an employee
-          // Add your code here
-          break;
-        case 'Update employee role':
-          // Implement logic to update an employee role
-          // Add your code here
-          break;
-        case 'View all roles':
-          // Implement logic to view all roles
-          // Add your code here
-          break;
-        case 'Add role':
-          // Implement logic to add a role
-          // Add your code here
-          break;
+          case 'Add employee':
+            // Implement logic to add an employee
+            inquirer
+              .prompt([
+                {
+                  type: 'input',
+                  name: 'firstName',
+                  message: 'Enter the first name of the employee:',
+                },
+                {
+                  type: 'input',
+                  name: 'lastName',
+                  message: 'Enter the last name of the employee:',
+                },
+                {
+                  type: 'input',
+                  name: 'jobTitle',
+                  message: 'Enter the job title of the employee:',
+                },
+                {
+                  type: 'input',
+                  name: 'department',
+                  message: 'Enter the department of the employee:',
+                },
+                {
+                  type: 'input',
+                  name: 'salary',
+                  message: 'Enter the salary of the employee:',
+                },
+              ])
+              .then((employeeData) => {
+                Employee.create(employeeData)
+                  .then(() => {
+                    console.log('Employee added successfully.');
+                    mainMenu(); // Return to main menu
+                  })
+                  .catch((error) => {
+                    console.error('Error adding employee:', error);
+                    mainMenu(); // Return to main menu
+                  });
+              });
+            break;
+          
+          case 'Update employee role':
+            // Implement logic to update an employee role
+            // You can prompt the user for employee ID and new role details, then update the employee record in the database
+            // Add your code here
+            break;
+          
+          case 'View all roles':
+            // Implement logic to view all roles
+            Role.findAll().then((roles) => {
+              console.log('All roles:');
+              console.log(roles);
+              mainMenu(); // Return to main menu
+            });
+            break;
+          
+          case 'Add role':
+            // Implement logic to add a role
+            inquirer
+              .prompt([
+                {
+                  type: 'input',
+                  name: 'title',
+                  message: 'Enter the title of the role:',
+                },
+                {
+                  type: 'input',
+                  name: 'salary',
+                  message: 'Enter the salary of the role:',
+                },
+                {
+                  type: 'input',
+                  name: 'departmentId',
+                  message: 'Enter the department ID for the role:',
+                },
+              ])
+              .then((roleData) => {
+                Role.create(roleData)
+                  .then(() => {
+                    console.log('Role added successfully.');
+                    mainMenu(); // Return to main menu
+                  })
+                  .catch((error) => {
+                    console.error('Error adding role:', error);
+                    mainMenu(); // Return to main menu
+                  });
+              });
+            break;
         case 'View all departments':
           // Implement logic to view all departments
           Department.findAll().then((departments) => {
